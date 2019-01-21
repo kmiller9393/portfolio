b1e725be153144a8f72566e90ee185470f3b6d66
Kurt Miller <kmiller9393@gmail.com>
2019-01-21 14:24:12 -0700

    Update App.js styling, wrap application in Switch component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Home from '../../components/Home/Home';
 import About from '../../components/About/About';
 import Projects from '../../components/Projects/Projects';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/projects" component={Projects} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+        </Switch>
       </div>
     );
   }
