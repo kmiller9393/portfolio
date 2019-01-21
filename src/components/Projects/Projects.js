1cb6dcc872f5b2b4cc0504f99d3e21e30bfdc769
Kurt Miller <kmiller9393@gmail.com>
2019-01-21 14:24:59 -0700

    Add four projects descriptions and links to Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,12 +1,256 @@
 import React, { Component } from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+import logo from '../../images/KM.svg';
+import github from '../../images/github-logo.svg';
+import orbital from '../../images/orbital.svg';
+import murrayTracker from '../../images/murray-tracker-home.svg';
+import swapibox from '../../images/swapibox-home.svg';
+import weathrly from '../../images/weathrly-home.svg';
 import './Projects.css';
 
-export default class Projects extends Component {
+class Projects extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstView: false,
+      secondView: false,
+      thirdView: false,
+      fourthView: false
+    };
+  }
+
+  routeHome = () => {
+    const { history } = this.props;
+
+    history.push('/');
+  };
+
+  toggleDetailView = e => {
+    const { name, classList } = e.target;
+
+    if (!this.state[name]) classList.add('active-button');
+    else classList.remove('active-button');
+
+    this.setState(prevState => ({
+      [name]: !prevState[name]
+    }));
+  };
+
   render() {
+    const { firstView, secondView, thirdView, fourthView } = this.state;
+
     return (
       <div className="projects-container">
-        <h2>Projects Here!</h2>
+        <header className="projects-header-container">
+          <img
+            className="logo"
+            src={logo}
+            alt="home logo"
+            onClick={this.routeHome}
+          />
+          <div className="projects-link-container">
+            <NavLink className="projects-link link-1" to="/">
+              Home
+            </NavLink>
+            <NavLink className="projects-link link-2" to="/about">
+              About
+            </NavLink>
+          </div>
+        </header>
+        <main className="main-projects-container">
+          <h1 className="projects-main-header">Projects</h1>
+          <section className="project-container">
+            <h2 className="project-header">Orbital</h2>
+            <img className="project-image" src={orbital} alt="orbital" />
+            <div className="button-launch-container">
+              <button
+                className="details-button"
+                onClick={e => this.toggleDetailView(e)}
+                name="firstView"
+              >
+                {!firstView ? 'More Details' : 'Hide Details'}
+              </button>
+              <a
+                className="launch-link"
+                href="https://orbital-app.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Launch App
+              </a>
+              <a
+                href="https://github.com/kmiller9393/orbital"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img className="github-logo" src={github} alt="github" />
+              </a>
+            </div>
+            {firstView && (
+              <article className="description-container">
+                <h3 className="description-header">Description</h3>
+                <p className="project-description">
+                  Orbital is a mobile application that gives users an easy and
+                  appealing way to learn more about the universe. There is a
+                  Review section, where users can review flashcards and read
+                  more about different space topics, a Trivia section, where
+                  users can put their space knowledge to the test and a Current
+                  Events section, where users can stay up-to-date with space
+                  events and continue to expand their space knowledge thanks to
+                  NASA's Astronomy Picture of the Day API.
+                </p>
+                <h3 className="description-header">Tech Stack</h3>
+                <p className="tech-stack-text">
+                  React.js, Redux, React Router, Jest, Enzyme
+                </p>
+              </article>
+            )}
+          </section>
+          <section className="project-container">
+            <h2 className="project-header">MurrayTracker</h2>
+            <img
+              className="project-image"
+              src={murrayTracker}
+              alt="movie tracker"
+            />
+            <div className="button-launch-container">
+              <button
+                className="details-button"
+                onClick={e => this.toggleDetailView(e)}
+                name="secondView"
+              >
+                {!secondView ? 'More Details' : 'Hide Details'}
+              </button>
+              <a
+                className="launch-link"
+                href="https://star-wars-api-km.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Launch App
+              </a>
+              <a
+                href="https://github.com/kmiller9393/movie-tracker"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img className="github-logo" src={github} alt="github" />
+              </a>
+            </div>
+            {secondView && (
+              <article className="description-container">
+                <h3 className="description-header">Description</h3>
+                <p className="project-description">
+                  MurrayTracker is an application that uses The Movie Database
+                  API to give the user access to a variety of Bill Murray movies
+                  as well as the ability to login and add specific titles of
+                  their choice to their favorites. If a user clicks on a
+                  particular movie card, they can view a synposis of that movie,
+                  its release date and its IMDb rating.
+                </p>
+                <h3 className="description-header">Tech Stack</h3>
+                <p className="tech-stack-text">
+                  React.js, Redux, React Router, Node.js, PostgreSQL, Jest,
+                  Enyzme
+                </p>
+              </article>
+            )}
+          </section>
+          <section className="project-container">
+            <h2 className="project-header">SWAPI-Box</h2>
+            <img className="project-image" src={swapibox} alt="star wars api" />
+            <div className="button-launch-container">
+              <button
+                className="details-button"
+                onClick={e => this.toggleDetailView(e)}
+                name="thirdView"
+              >
+                {!thirdView ? 'More Details' : 'Hide Details'}
+              </button>
+              <a
+                className="launch-link"
+                href="https://star-wars-api-km.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Launch App
+              </a>
+              <a
+                href="https://github.com/kmiller9393/swapibox"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img className="github-logo" src={github} alt="github" />
+              </a>
+            </div>
+            {thirdView && (
+              <article className="description-container">
+                <h3 className="description-header">Description</h3>
+                <p className="project-description">
+                  SWAPIBox is a web application that utilizes the SWAPI (Star
+                  Wars API) to display information about various people, planets
+                  and vehicles found in a galaxy far, far away. Users can view
+                  and add these people, planets and vehicles to their favorites
+                  as well. This application leverages asynchronous JavaScript
+                  and ES6 to efficiently fetch and display Star Wars data found
+                  within nested fetch calls for a beautifully interactive User
+                  Experience.
+                </p>
+                <h3 className="description-header">Tech Stack</h3>
+                <p className="tech-stack-text">
+                  React.js, React Router, Jest, Enzyme
+                </p>
+              </article>
+            )}
+          </section>
+          <section className="project-container">
+            <h2 className="project-header">Weathrly</h2>
+            <img className="project-image" src={weathrly} alt="weatherly" />
+            <div className="button-launch-container">
+              <button
+                className="details-button"
+                onClick={e => this.toggleDetailView(e)}
+                name="fourthView"
+              >
+                {!fourthView ? 'More Details' : 'Hide Details'}
+              </button>
+              <a
+                className="launch-link"
+                href="https://star-wars-api-km.herokuapp.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Launch App
+              </a>
+              <a
+                href="https://github.com/kmiller9393/weathrly"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img className="github-logo" src={github} alt="github" />
+              </a>
+            </div>
+            {fourthView && (
+              <article className="description-container">
+                <h3 className="description-header">Description</h3>
+                <p className="project-description">
+                  Weathrly is a dynamic and real-time weather application that
+                  uses weather data fetched from the Weather Underground API.
+                  Users can search for a city of their choosing and submit a
+                  search to view the current, hourly and ten day weather
+                  forecast for that particular city. Weathrly also implements a
+                  custom binary search trie and functionality to autocomplete a
+                  user's search for cities around the nation.
+                </p>
+                <h3 className="description-header">Tech Stack</h3>
+                <p className="tech-stack-text">React.js, Jest and Enzyme</p>
+              </article>
+            )}
+          </section>
+        </main>
       </div>
     );
   }
 }
+
+export default withRouter(Projects);
