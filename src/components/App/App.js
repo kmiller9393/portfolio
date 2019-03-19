388ce9ec6f944cef906b466977ea5a7135905cd2
kurt miller <kmiller9393@gmail.com>
2019-03-19 11:12:59 -0600

    Make App.js a functional component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from '../../components/Home/Home';
 import About from '../../components/About/About';
 import Projects from '../../components/Projects/Projects';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/projects" component={Projects} />
-        </Switch>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/projects" component={Projects} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
